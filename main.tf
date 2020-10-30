5a7f656eee7690d1ef506ff1a49799da0fe840f7
Zizu12323 <73708264+Zizu12323@users.noreply.github.com>
2020-10-30 21:32:16 +0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,18 @@
 provider "aws" {
-  profile = "default"
   region = "us-east-2"
+}
+
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+
+resource "aws_instance" "one" {
+  ami = "ami-03657b56516ab7912"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "TerraTesting"
+  }
 }
